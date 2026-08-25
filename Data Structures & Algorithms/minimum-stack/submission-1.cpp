9b8b53f3e61f4b63bf70176b474bcbbdaacbc6fc
Akarsh Tripathi <akarshtripathi111111@gmail.com>
2026-08-25 23:14:10 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+public:
+    vector<int>st;
+    vector<int>minSt;
+   
+    MinStack() {}
+    
+    void push(int val) {
+        st.push_back(val);
+        if (minSt.empty() || val <= minSt.back()) {
+            minSt.push_back(val);
+        }
+    }
+    
+    void pop() {
+        if(!st.empty()){
+            if(minSt.back()==st.back()){ 
+                minSt.pop_back();
+            }
+            st.pop_back();
+        }
+    }
+    
+    int top() {
+       return st.back();
+    }
+    
+    int getMin() {
+        
+       return minSt.back();
+        
+    }
+};
